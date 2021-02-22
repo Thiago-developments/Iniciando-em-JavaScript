b2d96a988725552d2e39626909299280b94845d5
Thiagao <thiago.sousa@fcamara.com.br>
2021-02-22 17:34:24 -0300

    Capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,16 +6,21 @@ botaoAdicionar.addEventListener("click", function () {
 
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
   xhr.addEventListener("load", function () {
-    console.log(xhr.responseText);
-    let resposta = xhr.responseText;
-    let pacientes = JSON.parse(resposta);
-    console.log(pacientes);
+    let erroAjax = document.querySelector("#erro-ajax");
+    if (xhr.status == 200) {
+      erroAjax.classList.add("invisivel");
+      console.log(xhr.responseText);
+      let resposta = xhr.responseText;
+      let pacientes = JSON.parse(resposta);
+      console.log(pacientes);
 
-    pacientes.forEach(function(paciente) {
+      pacientes.forEach(function (paciente) {
         adicionaPacienteNaTabela(paciente);
-    });
-
-
+      });
+    } else {
+      console.log(xhr.status);
+      erroAjax.classList.remove("invisivel");
+    }
   });
   xhr.send();
 });
